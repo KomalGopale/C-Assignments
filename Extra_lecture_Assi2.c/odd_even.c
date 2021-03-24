6600dc0ac42086dff49f6c12e938012c86549a97
Komal_Gopale <79790735+KomalGopale@users.noreply.github.com>
2021-03-24 23:27:33 +0530

    Add files via upload

---
+++ Extra_lecture_Assi2.c/odd_even.c
@@ -0,0 +1,17 @@
+            /*program to check wether entered number is even or odd*/
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter number that you want to check: ");
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+      printf("%d is an even number",number);
+    }
+    else
+    {
+      printf("%d is an odd number",number);
+    }
+return 0;
+}
